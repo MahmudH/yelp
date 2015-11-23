6f7dc70ee357c5c67a05eefc957a04a4b4bc29be
mahmudh <mahmudhssn246@gmail.com>
2015-11-23 15:03:43 +0000

    link to create new restaurant

--- yelp_clone/db/schema.rb
+++ yelp_clone/db/schema.rb
@@ -23,9 +23,4 @@ ActiveRecord::Schema.define(version: 20151123144004) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "students", id: false, force: :cascade do |t|
-    t.integer "id"
-    t.string  "name"
-  end
-
 end
