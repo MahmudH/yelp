94a0cc29bb9011be40ce86d15a6a448bed083495
mahmudh <mahmudhssn246@gmail.com>
2015-11-27 15:30:24 +0000

    test to add average rating

--- yelp_clone/spec/models/restaurant_spec.rb
+++ yelp_clone/spec/models/restaurant_spec.rb
@@ -5,4 +5,13 @@ describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
 
   it { is_expected.to belong_to :user }
+
+  describe '#average rating' do
+    context 'no reviews' do
+      it 'returns "N/A" when there are no reviews' do
+        restaurant = Restaurant.create(name: 'The Ivy')
+        expect(restaurant.average_rating).to eq 'N/A'
+      end
+    end
+  end
 end
